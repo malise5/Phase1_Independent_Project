380e9e47dce1d8451d7421b66a9e8c8bc9dda697
Halkano <kudezx@gmail.com>
2022-09-04 04:10:05 +0300

    Updated

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Phase1_Independent_Project
+``` console.log(data[0]); //we Acces object name data
+        console.log(data[0].capital[0]); //helps to acces the Capital City
+        console.log(data[0].flags.svg); //The flag is captured here
+        console.log(data[0].name.common); // The country name is captured here
+        console.log(data[0].continents[0]); //acccess the specific Continent the country belongs to
+        console.log(Object.keys(data[0].currencies)[0]); //currency money for each country
+        console.log(Object.values(data[0].languages).toString().split(",").join(", ")); //Display languages spoken by the specific country
+        console.log(data[0].maps.googleMaps); // google maps for each country
+ ````
--- index.js
+++ index.js
@@ -1,16 +1,72 @@
-let searchBtn = document.querySelector("#search-btn");
-let country = document.querySelector("#country");
+const init = () => {
 
-searchBtn.addEventListener("click",()=>{
-    let countryName = "Kenya";
-    let url = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-    console.log(url);
-    fetch(url).then((response)=>response.json()).then((data)=>{
-        console.log(data[0]); //we Acces object name data
-        console.log(data[0].capital[0]); //helps to acces the Capital City
-        console.log(data[0].flags.svg); //The flag is captured here
-        console.log(data[0].name.common); // The country name is captured here
-        console.log(data[0].continents[0]); //acccess the specific Continent the country belongs to
-        console.log(Object.keys(data[0].currencies)[0]); //currency money for each country
-    })
-})
+
+    let searchBtn = document.querySelector("#search-btn");
+    let country = document.querySelector("#country");
+
+    searchBtn.addEventListener("click",()=>{
+        let countryName = country.value;
+        let url = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+        console.log(url);
+        fetch(url).then((response)=>response.json()).then((data)=>{
+            result.innerHTML = `
+            <img src="${data[0].flags.svg}" class="flag-img">
+            <h2>${data[0].name.common}</h2>  
+            <div class = "wrapper">
+                <div class ="data-wrapper">
+                    <h4>Capital:</h4>
+                    <span>${data[0].capital[0]}</span>
+                </div>
+            </div>  
+
+            <div class = "wrapper">
+                <div class ="data-wrapper">
+                    <h4>Continent:</h4>
+                    <span>${data[0].continents[0]}</span>
+                </div>
+            </div> 
+
+
+            <div class = "wrapper">
+                <div class ="data-wrapper">
+                    <h4>Currency:</h4>
+                    <span>${Object.keys(data[0].currencies)[0]}</span>
+                </div>
+            </div>
+
+            <div class = "wrapper">
+                <div class ="data-wrapper">
+                    <h4>Continent:</h4>
+                    <span>${data[0].continents[0]}</span>
+                </div>
+            </div> 
+
+            <div class = "wrapper">
+                <div class ="data-wrapper">
+                    <h4>Language:</h4>
+                    <span>${Object.values(data[0].languages).toString().split(",").join(", ")}</span>
+                </div>
+            </div> 
+
+            <div class = "wrapper">
+                <div class ="data-wrapper">
+                    <h4>Population:</h4>
+                    <span>${data[0].population}</span>
+                </div>
+            </div> 
+            `;
+
+        }).catch(() => {
+            if (countryName.length == 0){
+                result.innerHTML =`<h3>The input field can not be empty</h3>`
+            }
+            else{
+                result.innerHTML =  `<h3>Please Enter a Valid country name.</h3>`
+            }
+        })
+    });
+}
+
+document.addEventListener("DOMContentLoaded", init);
+
+
